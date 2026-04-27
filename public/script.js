a891aabd4c5428c5d8a1f5782489dd19ea4407df
Dev-ahmedsiii <adf2759009@gmail.com>
2026-04-27 16:21:19 +0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -5,9 +5,24 @@ let a=document.getElementById("a").value;
 
 let c=10*w+6.25*h-5*a+5;
 
-document.getElementById("r").innerText="🔥 احتياجك اليومي: "+Math.round(c)+" سعرة";
+document.getElementById("r").innerText="🔥 "+Math.round(c)+" سعرة يومياً";
 }
 
-function toggleDark(){
-document.body.classList.toggle("dark");
+function aiAdvice(){
+let w=document.getElementById("w").value;
+
+if(!w){
+document.getElementById("ai").innerText="أدخل البيانات أولاً";
+return;
+}
+
+if(w < 60){
+document.getElementById("ai").innerText="🟡 تحتاج زيادة أكل صحي";
+}
+else if(w < 80){
+document.getElementById("ai").innerText="🟢 وزنك ممتاز";
+}
+else{
+document.getElementById("ai").innerText="🔴 قلل الدهون وابدأ رياضة";
+}
 }
